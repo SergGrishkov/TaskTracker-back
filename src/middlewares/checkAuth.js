121794c24736f6a61a3897dbecd32f86e8550f21
SergGrishkov <serg.grishkov@gmail.com>
2024-06-07 21:16:35 +0300

    Update checkAuth.js

--- src/middlewares/checkAuth.js
+++ src/middlewares/checkAuth.js
@@ -2,26 +2,26 @@ import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 import HttpError from "../helpers/HttpError.js";
 
-export const checkAuth = async (req, _, next) => {
+export const checkAuth = async (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
-      throw HttpError(401);
+      return res.status(401).send({ message: "Invalid token" });
     }
     const [bearer, token] = authHeader.split(" ");
     if (bearer !== "Bearer") {
-      throw HttpError(401);
+      return res.status(401).send({ message: "Invalid token" });
     }
 
     jwt.verify(token, process.env.JWT_SECRET, async (err, decode) => {
       if (err) {
-        throw HttpError(401);
+        return res.status(401).send({ message: "Invalid token" });
       }
 
       const user = await User.findById(decode.id);
 
       if (!user || user.token !== token) {
-        throw HttpError(401);
+        return res.status(401).send({ message: "Invalid token" });
       }
 
       req.user = {
