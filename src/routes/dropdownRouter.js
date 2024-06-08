1aaab2ade56bbb2c81f8303aa788211a66670d0d
Maksimka66 <maxfese@gmail.com>
2024-06-08 23:16:44 +0300

    some changes

--- src/routes/dropdownRouter.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { chooseColumn } from "../controllers/dropdownControllers.js";
-import isValidId from "../helpers/validateId.js";
-
-const dropdownRouter = express.Router();
-
-dropdownRouter.patch("/:id", isValidId, chooseColumn);
-
-export default dropdownRouter;
