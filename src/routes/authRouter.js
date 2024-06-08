5a5a8fb2480f63501ae2449b66eba30f702eedb3
lysbrodya <lysbrodya@gmail.com>
2024-06-08 21:39:03 +0300

    curent

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,20 +1,18 @@
-import express from 'express';
+import express from "express";
 import validateBody from "../helpers/validateBody.js";
-import {
-  registerUserSchema,
-  loginUserSchema,
-} from "../schemas/authSchemas.js";
+import { registerUserSchema, loginUserSchema } from "../schemas/authSchemas.js";
 import {
   register,
   login,
   logout,
+  current,
 } from "../controllers/authControllers.js";
-import {checkAuth} from '../middlewares/checkAuth.js';
+import { checkAuth } from "../middlewares/checkAuth.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/register", validateBody(registerUserSchema), register);
 authRouter.post("/login", validateBody(loginUserSchema), login);
 authRouter.post("/logout", checkAuth, logout);
-
+authRouter.get("/current", checkAuth, current);
 export default authRouter;
