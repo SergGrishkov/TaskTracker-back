f20d209faf0287c31d842f7a9778c622a110d8c1
SergGrishkov <43968618+SergGrishkov@users.noreply.github.com>
2024-06-11 00:44:27 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const options = {
   explorer: true,
 };
 
-app.use(morgan("tiny"));
+app.use(morgan("common"));
 app.use(cors());
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
