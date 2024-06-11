7d36073e163c3940eb3e6a21d031e7befb4b9dbf
Maksimka66 <maxfese@gmail.com>
2024-06-11 19:27:15 +0300

    Update boardsControllers.js

--- src/controllers/boardsControllers.js
+++ src/controllers/boardsControllers.js
@@ -118,13 +118,16 @@ export const deleteBoard = errorWrapper(async (req, res) => {
 });
 
 export const getAllBackgrounds = errorWrapper(async (req, res) => {
-  const { id: userId } = req.user;
+  console.log(2);
 
-  if (!userId) {
-    throw HttpError(404);
-  }
+  // const { _id: userId } = req.user;
+
+  // if (!userId) {
+  //   throw HttpError(404);
+  // }
 
   const board = await Board.find({ userId });
+  console.log(2);
 
   res.json(board);
 });
