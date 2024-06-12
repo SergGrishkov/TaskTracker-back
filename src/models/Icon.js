3285bb435bdc243b585c76b6f65520eb4e8a51fc
Maksimka66 <maxfese@gmail.com>
2024-06-12 05:07:02 +0300

    Update Icon.js

--- src/models/Icon.js
+++ src/models/Icon.js
@@ -1,21 +1,9 @@
 import mongoose from "mongoose";
 
 const iconSchema = mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "user",
-    required: true,
-  },
   path: {
     type: String,
     required: true,
-    validate: {
-      validator: function (v) {
-        return /^(https?:\/\/).*$/i.test(v);
-      },
-      message: (props) =>
-        `${props.value} is not a valid path! Please provide a valid URL path.`,
-    },
   },
 });
 
