eb4ca0628a9dd7c08eb09413913d7d780b75f4f2
SergGrishkov <serg.grishkov@gmail.com>
2024-06-15 18:54:00 +0300

    Update authControllers.js

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -38,7 +38,6 @@ export const register = errorWrapper(async (req, res, next) => {
 export const login = errorWrapper(async (req, res, next) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
-  // const theme = user.theme;
 
   if (!user) {
     throw HttpError(401, "Email or password is wrong");
